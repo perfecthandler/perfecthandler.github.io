096f27f8b7bab6c33d5e9885fcbb8427b7923502
perfecthandler <perfecthandler@gmail.com>
2023-11-15 00:15:36 +0530

    ee

--- location-ar6/map.js
+++ location-ar6/map.js
@@ -92,3 +92,11 @@ window.addEventListener('resize', function () {
 //       map.invalidateSize();
 //     });
 //   });
+
+// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+// const points = [];
+// points.push( new THREE.Vector3( - 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, 10, 0 ) );
+// points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+// const geometry = new THREE.BufferGeometry().setFromPoints( points );
